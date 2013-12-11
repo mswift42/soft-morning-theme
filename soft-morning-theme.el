7b545efa1b5c0ecc387a0011b9305c31b2f68ba7
mswift42 <martin.haesler@gmail.com>
2013-12-11 21:42:44 +0000

    Added face for info+ mode.

--- soft-morning-theme.el
+++ soft-morning-theme.el
@@ -95,6 +95,8 @@
 	'(gnus-header-content ((t (:foreground "#708090"))))
 	'(gnus-server-agent-face ((t (:foreground "#626160"))))
 	'(magit-item-highlight ((t (:background "#d3d2d1"))))
+	'(ac-completion-face ((t (:underline t :foreground "#ff0000"))))
+	'(info-string ((t (:foreground "#3450a2"))))
 	'(slime-repl-inputed-output-face ((t (:foreground "#727170")))))
 
 ;;;###autoload
